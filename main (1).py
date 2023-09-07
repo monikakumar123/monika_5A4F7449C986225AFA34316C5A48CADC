2b74980459f6d862471fc7cd9598b17c8347c0d6
monikakumar123 <144248057+monikakumar123@users.noreply.github.com>
2023-09-07 11:53:22 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,9 @@
+pr=int(input("enter principal"))
+rate=int(input("enter rate"))
+time=int(input("enter time"))
+si=(pr*rate*time)/100
+amount=pr+si
+print("simple interest:",si)
+print("amount payable:",amount)
+
+
---
+++ main.py
@@ -0,0 +1,9 @@
+def increment(num):
+  print("Running 1000 lines of code")
+  return num + 1
+
+
+print(increment(1))
+print(increment(2))
+print(increment(3))
+print(increment(1))
